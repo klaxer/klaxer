f0edf848fca7505548f37eabf9b4eeaac4d4dc2d
Justin Shelton <babatheox@gmail.com>
2017-05-19 16:34:57 -0700

    Update authors

--- klaxer/__init__.py
+++ klaxer/__init__.py
@@ -1,6 +1,6 @@
 """Entry point for all things Klaxer"""
 
-__author__ = 'Aru Sahni'
+__author__ = 'Aru Sahni, Kevin Dwyer, Justin Shelton, Dylan Bernard, et al'
 __version__ = '0.0.1'
 __licence__ = 'MIT'
 
