bcf290001ac58af0cef4e136587145eda511f80e
Will Schneider <william.schneider@digitalglobe.com>
2017-05-21 00:33:59 +0000

    Fix spelling, add name to authors, add .idea/ to .gitignore

--- klaxer/__init__.py
+++ klaxer/__init__.py
@@ -1,7 +1,7 @@
 """Entry point for all things Klaxer"""
 
-__author__ = 'Aru Sahni, Kevin Dwyer, Justin Shelton, Dylan Bernard, et al'
+__author__ = 'Aru Sahni, Kevin Dwyer, Justin Shelton, Dylan Bernard, Will Schneider, et al'
 __version__ = '0.0.1'
-__licence__ = 'MIT'
+__license__ = 'MIT'
 
 APP_NAME = 'Klaxer'
